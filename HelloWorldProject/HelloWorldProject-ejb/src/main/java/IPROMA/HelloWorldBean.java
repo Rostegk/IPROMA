698a5675827086d9a9f73efd76666f4596e50fa3
Robin Stegk <rostegk@googlemail.com>
2011-07-27 09:40:06 +0200

    Hello World with EJB

---
+++ HelloWorldProject/HelloWorldProject-ejb/src/main/java/IPROMA/HelloWorldBean.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package IPROMA;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author robin
+ */
+@Stateless
+public class HelloWorldBean {
+
+    public String getName() {
+        return "Hello World";
+    }
+
+    
+    // Add business logic below. (Right-click in editor and choose
+    // "Insert Code > Add Business Method")
+    
+}
